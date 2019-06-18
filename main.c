d700be776f3086d78327df83c9763603fbb3f7a2
Karim Echchennouf <k.echchennouf@gmail.com>
2019-06-18 15:17:10 +0200

    refactor: outsource functions

--- main.c
+++ main.c
@@ -14,51 +14,57 @@
 
 int main()
 {
-  int i = 0;
   int arr[9][9];
+  int ele;
 
-int indexOf(int array[], int ele) {
-  for(int x= 0; x < 9; x++){
-      if(array[x] == ele){
-        return 1;
-      }
+  int indexOf(int array[], int ele) {
+    for(int x= 0; x < 9; x++){
+        if(array[x] == ele){
+          return x;
+        }
+    }
+    return -1;
   }
-  return 0;
-}
 
-  for(int x= 0; x < 9; x++){
-    for(int y =0; y< 9; y++){
-      int ele = rand() % 10;
-      if(indexOf(arr[x], ele)> 0){
-        y--;
-      } else{
-        arr[x][y] = ele;
+  void generateGameData(int a[][9]){
+    for(int x= 0; x < 9; x++){
+      for(int y =0; y< 9; y++){
+        ele = rand() % 10;
+        if(indexOf(a[x], ele)>= 0){ // element darf nur einmal vorkommen.
+          y--;
+          continue;
+        } 
+          a[x][y] = ele;
       }
     }
   }
 
-  int x = 0;
-  int y = 0;
+  void renderCourt(int arr[][9]){
+      int x = 0;
+      int y = 0;
 
-    i = 0;
-    system("clear");
-    printf("%s+---+---+---+---+---+---+---+---+---+\n", KRED);
-    for (i; i < 9; i++) {
-      for (int j=0; j < 9; j++) {
-            if(j % 3 == 0){
-              printf("%s| ", KRED ,arr[i][j]);
-            } else {
-              printf("%s| ", KWHT ,arr[i][j]);
-            }
-            printf("%s%d ", KWHT ,arr[i][j]);
-      }
-      printf("%s|\n", KRED);
-      if((i+1) % 3 == 0){
-        printf("%s+---+---+---+---+---+---+---+---+---+\n", KRED);
-      } else{
-        printf("%s+---+---+---+---+---+---+---+---+---+\n", KWHT);
-      }
+      int i = 0;
+      system("clear");
+      printf("%s+---+---+---+---+---+---+---+---+---+\n", KRED);
+      for (i; i < 9; i++) {
+        for (int j=0; j < 9; j++) {
+              if(j % 3 == 0){
+                printf("%s| ", KRED ,arr[i][j]);
+              } else {
+                printf("%s| ", KWHT ,arr[i][j]);
+              }
+              printf("%s%d ", KWHT ,arr[i][j]);
+        }
+        printf("%s|\n", KRED);
+        if((i+1) % 3 == 0){
+          printf("%s+---+---+---+---+---+---+---+---+---+\n", KRED);
+        } else{
+          printf("%s+---+---+---+---+---+---+---+---+---+\n", KWHT);
+        }
     }
-    sleep(1);
+  }
+
+  generateGameData(arr);
+  renderCourt(arr);
   return 0;
 }
