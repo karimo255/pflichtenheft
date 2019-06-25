4365ffbf18acc357871763b1109eef65dab0e165
Karim Echchennouf <k.echchennouf@gmail.com>
2019-06-25 15:10:45 +0200

    feat: implementing of database services

---
+++ headers/services/connection.h
@@ -0,0 +1,7 @@
+//
+// Created by karim on 25.06.19.
+//
+
+#include "../../libs/sqlite3.h"
+
+extern sqlite3 *connection;
---
+++ src/services/user_service.c
@@ -0,0 +1,26 @@
+//
+// Created by karim on 25.06.19.
+//
+#include "../../headers/services/user_service.h"
+#include "../../headers/services/connection.h"
+#include <stdio.h>
+
+
+
+char sql[200];
+char *zErrMsg;
+sqlite3 *connection;
+
+
+int registerUser(char username[]){
+    sprintf(sql, "INSERT INTO `User` (Name) VALUES(\"%s\");", username);
+
+    int rc = sqlite3_exec(connection, sql, NULL, NULL, &zErrMsg);
+    printf("%s\n", sql);
+
+    if(!rc == SQLITE_OK){
+        return -1;
+    } else{
+        return 0;
+    }
+}
